8045d00d0426f53044786c2fc8f02218a001de5f
red_bobcat <redbobcat@github.com>
2019-04-30 02:31:44 +0400

    Makefile added, BMP edited

--- BMP280/BMP280.h
+++ BMP280/BMP280.h
@@ -4,16 +4,22 @@
 /***********************************************************************
  *Libruary to interface with BMP280 Bosch sensor
  *to use in warious projects
- * 
+ *
  * Use with included libs, and all will be OK!
  * At list I think it will be
+ * use like this
+ bmp280_init();
+ //then
+ bmp280_measure();
+ ultoa(bmp280_temp, buffer, 10);
+ ultoa(bmp280_pres, buffer, 10); for output ASCII
  **********************************************************************/
- 
+
  #include <avr/io.h>
- 
+
  #define BMP280_ADDR 0x76 //0x77 (to Vcc) or 0x76 (to GND)
- 
-  
+
+
 //here is oversampling controls in CONTROL reg
 #define BMP280_PRES_OVERSAMPLING 0b100 //place here table of P oversamp
 /* 0b000 Skiped
@@ -23,7 +29,7 @@
  * 0b100 Oversampling x8
  * 0b101 Oversampling x16
  */
- 
+
 #define BMP280_TEMP_OVERSAMPLING 0b100 //place here table of T oversamp
 /* 0b000 Skiped
  * 0b001 Oversampling x1
@@ -38,7 +44,7 @@
  * 0b01 or 0b10 Forced mode (one-shot)
  * 0b11 Normal mode (Continious mesuring)
  */
- 
+
 //Here is config settings in CONFIG register
 #define BMP280_TIME_STANDBY 0b100 //Stand-by in normal mode
 /* 0b000 0.5ms
@@ -62,7 +68,7 @@
 
 uint8_t bmp280_init (void); //base initialization, 1 is sucsess
  uint8_t bmp280_get_status (void); //get status register
- void bmp280_set_config (void); //set config reg 
+ void bmp280_set_config (void); //set config reg
  void bmp280_set_control (void); //set control red (oversampling i.e)
  void bmp280_measure (void); //get T, P and compute it readable format
  //#define bmp280_get_temperature() {bmp280_temp} //returns temperature
